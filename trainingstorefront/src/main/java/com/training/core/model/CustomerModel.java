8816213d8fd7e8560b13576036ffc1bc7c54d413
vamsi <vamsichadaram@luminescent.digital>
2018-04-07 23:16:21 +0530

    changes

--- trainingstorefront/src/main/java/com/training/core/model/CustomerModel.java
+++ trainingstorefront/src/main/java/com/training/core/model/CustomerModel.java
@@ -1,7 +1,6 @@
 package com.training.core.model;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -12,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
--- trainingstorefront/src/main/java/com/training/core/model/ExperienceModel.java
+++ trainingstorefront/src/main/java/com/training/core/model/ExperienceModel.java
@@ -1,6 +1,5 @@
 package com.training.core.model;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -13,50 +12,52 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name="EXPERIENCE")
-public class ExperienceModel {
-	    @Id
-		@GeneratedValue
-		private Long id;
-		
-		@Column(name="YEARS")
-		private Integer years;
-		
-		@Column(name="MONTHS")
-		private Integer months;
-		
-		@OneToMany
-		@JoinColumn(name="fields")
-		private List<FieldModel> fields;
-		
-		@Column(name="SUMMARY")
-		private String summary;
-		
-		@OneToOne
-		@JoinColumn(name="CUSTOMER")
-	   private CustomerModel customer;
+public class ExperienceModel 
+{
+	@Id
+	@GeneratedValue
+	private Long id;
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	@Column(name = "YEARS")
+	private Integer years;
 
-		public Integer getMonths() {
-			return months;
-		}
+	@Column(name = "MONTHS")
+	private Integer months;
 
-		public void setMonths(Integer months) {
-			this.months = months;
-		}
+	@OneToMany
+	@JoinColumn(name = "fields")
+	private List<FieldModel> fields;
 
-		public Integer getYears() {
-			return years;
-		}
-			public void setYears(Integer years) {
-				this.years= years;
-		
-}
+	@Column(name = "SUMMARY")
+	private String summary;
 
-			public void setSummary(String summary2) {
-				this.summary=summary;
-				
-			}
+	@OneToOne
+	@JoinColumn(name = "CUSTOMER")
+	private CustomerModel customer;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getMonths() {
+		return months;
+	}
+
+	public void setMonths(Integer months) {
+		this.months = months;
+	}
+
+	public Integer getYears() {
+		return years;
+	}
+
+	public void setYears(Integer years) {
+		this.years = years;
+
+	}
+
+	public void setSummary(String summary2) {
+		this.summary = summary;
+
+	}
 }
