2ec999654659b46cee2f2168394e6f0ce893d9e0
vamsi <vamsichadaram@luminescent.digital>
2018-04-15 02:11:44 +0530

    country

--- trainingstorefront/src/main/java/com/training/storefront/controllers/CountryController.java
+++ trainingstorefront/src/main/java/com/training/storefront/controllers/CountryController.java
@@ -7,24 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.training.core.data.CountryData;
-import com.training.core.model.AddressModel;
 import com.training.core.model.CountryModel;
 import com.training.core.service.CountryService;
 
 @Controller
-@RequestMapping("/coun")
-public class CountryController {
-	@Resource(name="countryService")
+@RequestMapping("/country")
+public class CountryController 
+{
+	@Resource(name = "countryService")
 	private CountryService countryService;
-    
-	@RequestMapping(value="/create", method= RequestMethod.POST)
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	@ResponseBody
-	public void createCountry(CountryData countryData)
+	public void createCountry(CountryData countryData) 
 	{
 		System.out.println(countryData);
-		CountryModel country= new CountryModel();
+		CountryModel country = new CountryModel();
 		country.setId(countryData.getId());
 		country.setIsoCode(countryData.getIsoCode());
 		country.setName(countryData.getName());
-	countryService.saveCountry(country);
-	}}
+		countryService.saveCountry(country);
+	}
+}
