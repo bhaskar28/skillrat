5922492e3cb3249e727554ece1cfd68305d00756
vamsi <vamsichadaram@luminescent.digital>
2018-04-15 10:16:29 +0530

    City added

---
+++ trainingstorefront/src/main/java/com/training/core/model/CityModel.java
@@ -0,0 +1,62 @@
+package com.training.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="NAME")
+public class CityModel 
+{
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name="ISO_CODE")
+	private String cityIsoCode;
+	
+	@Column(name="ISO_CODE")
+	private String name;
+
+	@OneToOne
+	@JoinColumn(name = "REGION")
+	private RegionModel region;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCityIsoCode() {
+		return cityIsoCode;
+	}
+
+	public void setCityIsoCode(String cityIsoCode) {
+		this.cityIsoCode = cityIsoCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public RegionModel getRegion() {
+		return region;
+	}
+
+	public void setRegion(RegionModel region) {
+		this.region = region;
+	}
+	
+	
+}
