162fe69dca2fc40c4d465e8059c20d1788cb1bfa
vamsi <tovamsich@gmail.com>
2018-05-04 10:57:16 +0530

    cities

--- trainingstorefront/src/main/java/com/training/storefront/controllers/CityController.java
+++ trainingstorefront/src/main/java/com/training/storefront/controllers/CityController.java
@@ -1,5 +1,6 @@
 package com.training.storefront.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.training.core.data.CityData;
 import com.training.core.model.CityModel;
 import com.training.core.service.CityService;
+import com.training.core.service.RegionService;
 
 @Controller
 @RequestMapping("/city")
@@ -17,6 +19,9 @@ public class CityController
 {
 	@Resource(name="cityService")
 	private CityService cityService;
+	
+	@Resource(name="regionService")
+	private RegionService regionService;
 	
 	@RequestMapping("/add")
 	public void addCity(CityData cityData)
@@ -31,7 +36,16 @@ public class CityController
 	public List<CityData> getCitiesForRegion(Long regionCode)
 	{
 		List<CityModel> cities= cityService.getAllCitiesForRegion(regionCode);
-		return null;
+		List<CityData> cityList= new ArrayList<CityData>();
+		for(CityModel city: cities)
+		{
+			CityData cityData= new CityData();
+			cityData.setIsoCode(city.getCityIsoCode());
+			cityData.setName(city.getName());
+			cityList.add(cityData);
+		}
+		
+		return cityList;
 	}
 }
 	
