8b083b25de1e41aa2d716521e18c4461addf2a3f
vamsi <tovamsich@gmail.com>
2018-06-06 11:51:06 +0530

    changes

--- trainingstorefront/src/main/java/com/training/storefront/controllers/ProductController.java
+++ trainingstorefront/src/main/java/com/training/storefront/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package com.training.storefront.controllers;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.training.core.data.PriceRowData;
 import com.training.core.data.ProductData;
-import com.training.core.model.PriceRowModel;
-import com.training.core.model.ProductModel;
 import com.training.core.query.data.ProductQueryData;
 import com.training.core.service.CurrencyService;
 import com.training.core.service.CustomerService;
@@ -56,50 +53,20 @@ public class ProductController
 		productFacade.saveProduct(productData);
 	}
 	
-	@RequestMapping(value="/customer-products", method= RequestMethod.GET)
+	@RequestMapping(value="/customer-products", method= RequestMethod.POST)
 	@ResponseBody
 	public List<ProductData> getCustomerProducts(Long customerId)
 	{
-		List<ProductModel> products=productService.getProductsByCustomer(customerId);
-		
-		List<ProductData> productsData=convert(products);
-		return productsData;
+		List<ProductData> products=productFacade.getProductsByCustomer(customerId);
+		return products;
 	}
 	
 	@RequestMapping(value="/customer-products", method= RequestMethod.POST)
 	@ResponseBody
 	public List<ProductData> getCustomerProducts(ProductQueryData productQuery)
 	{
-		List<ProductModel> products=productService.getProductsByCustomer(productQuery);
-		
-		List<ProductData> productsData=convert(products);
-		return productsData;
+		List<ProductData> products=productFacade.getProductsByCustomer(productQuery);
+		return products;
 	}
 
-	private List<ProductData> convert(List<ProductModel> products) 
-	{
-		List<ProductData> productsList= new ArrayList<ProductData>();
-		for(ProductModel product: products)
-		{
-			PriceRowModel priceRowModel= product.getPrice();
-			PriceRowData priceRow= new PriceRowData();
-			ProductData productData= new ProductData();
-			if(priceRowModel != null)
-			{
-				priceRow.setCurrencyCode(priceRowModel.getCurrency().getIsoCode());
-				priceRow.setFixedPrice(priceRowModel.getFixedPrice());
-				priceRow.setMaximum(priceRowModel.getMaximumPrice());
-				priceRow.setMinimum(priceRowModel.getMinimumPrice());
-				priceRow.setId(priceRowModel.getId());
-				productData.setPriceRow(priceRow);
-			}
-			
-			productData.setDescription(productData.getDescription());
-			productData.setName(productData.getName());
-			productData.setPriceRow(priceRow);
-			productsList.add(productData);
-		}
-		return productsList;
-	}
-	
 }
